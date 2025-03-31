6544ee2c930b97169924df24031452f52607b303
chanwon3144 <wkdcks3144@naver.com>
2025-03-31 18:10:37 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main(void)
+{
+	long long A, B, C;
+
+	scanf("%ld %ld %ld", &A, &B, &C);
+
+	printf("%ld\n", (A + B + C));
+
+
+	return 0;
+}
