45a7ee4f91b4b9c3d05c9e02a5987f92cdc54285
chanwon3144 <wkdcks3144@naver.com>
2025-04-02 09:03:42 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int T, i, a, b;
+	scanf("%d", &T);
+
+	for (i = 1; i <= T; i++)
+	{
+		scanf("%d %d", &a, &b);
+
+		if (a > 0 && b < 10)
+		{
+			printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+		}
+	}
+	return 0;
+}
