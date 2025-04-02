bf726182982d8e5886ca03006d0ba71f435e4fa2
chanwon3144 <wkdcks3144@naver.com>
2025-04-02 10:10:39 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int a, b;
+	
+
+	for (;;)
+	{
+		scanf("%d %d", &a, &b);
+
+		if (a == 0 && b == 0)
+		{
+			break;
+		}
+		
+		else
+		{
+			printf("%d\n", a + b);
+		}
+	}
+}
